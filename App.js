a75ea58bed0ad8aeab0525eaac076d87e288098d
Juan Velazquez <juandvelaz@gmail.com>
2021-01-24 17:35:54 -0600

    Feature: Implimented custom Card component in WelcomeScreen

--- App.js
+++ App.js
@@ -4,18 +4,27 @@ import { StyleSheet, View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import AppButton from './app/components/AppButton';
+import AppCard from './app/components/AppCard';
 
 export default function App() {
   // <WelcomeScreen />
   // <ViewImageScreen />;
   // <AppButton title="Login" />;
-  return <WelcomeScreen />;
+  return (
+    <View style={styles.container}>
+      <AppCard
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require('./app/assets/jacket.jpg')}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#f8f4f4',
+    padding: 20,
+    paddingTop: 100,
   },
 });
