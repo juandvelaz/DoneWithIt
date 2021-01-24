0d4ffd3c688d3e3b977a7d67886d11f0bfb00d9e
Juan Velazquez <juandvelaz@gmail.com>
2021-01-24 11:40:17 -0600

    Feature: Created a WelcomeScreen

--- App.js
+++ App.js
@@ -1,43 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-  ImageBackground,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import { useDeviceOrientation } from '@react-native-community/hooks';
+import { StyleSheet } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  const image = { uri: 'https://picsum.photos/1242/2688' };
-  const { portrait } = useDeviceOrientation();
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={image} style={styles.image}>
-        <SafeAreaView>
-          {portrait ? <Text style={styles.text}>Hello World!</Text> : <></>}
-        </SafeAreaView>
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000a0',
-  },
-});
+const styles = StyleSheet.create({});
