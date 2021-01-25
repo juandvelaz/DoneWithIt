c13522b460003feda169ce6cd296a7ea9f303b9c
Juan Velazquez <juandvelaz@gmail.com>
2021-01-25 10:23:12 -0600

    Task: Updated AppListItemDeleteAction to include an onPress parameter

--- app/components/AppListItemDeleteAction.js
+++ app/components/AppListItemDeleteAction.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import colors from '../config/colors';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-function AppListItemDeleteAction(props) {
+function AppListItemDeleteAction({ onPress }) {
   return (
-    <View style={styles.container}>
-      <MaterialCommunityIcons name="trash-can" size={35} color={colors.white} />
-    </View>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="trash-can"
+          size={35}
+          color={colors.white}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
