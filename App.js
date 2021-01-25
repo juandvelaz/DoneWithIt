6249aae4b43eafc8edd786955bb3c770877eaabe
Juan Velazquez <juandvelaz@gmail.com>
2021-01-25 11:11:21 -0600

    Task: Created AppIcon Component

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
-import MessagesScreen from './app/screens/MessagesScreen';
+import AppScreen from './app/components/AppScreen';
+import AppIcon from './app/components/AppIcon';
 
 export default function App() {
-  return <MessagesScreen></MessagesScreen>;
+  return (
+    <AppScreen>
+      <AppIcon
+        name="email"
+        size={50}
+        backgroundColor="red"
+        iconColor="white"
+      ></AppIcon>
+    </AppScreen>
+  );
 }
 
 const styles = StyleSheet.create({});
