b567f09348b92064f2f66ae71a2377e8088c2b36
Juan Velazquez <juandvelaz@gmail.com>
2021-01-24 21:44:50 -0600

    Task: Created ListItem components

--- app/components/AppListItem.js
+++ app/components/AppListItem.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, TouchableHighlight, View } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import colors from '../config/colors';
 import AppText from './AppText';
 
-function AppListItem({ title, subTitle, image }) {
+function AppListItem({ title, subTitle, image, onPress, renderRightActions }) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image} />
-      <View>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
-      </View>
-    </View>
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={image} />
+          <View>
+            <AppText style={styles.title}>{title}</AppText>
+            <AppText style={styles.subTitle}>{subTitle}</AppText>
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flexDirection: 'row' },
+  container: { flexDirection: 'row', padding: 15 },
   image: {
     width: 70,
     height: 70,
