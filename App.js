0fcb8169f9ff77d0cbcf7276f9db514800a621da
Juan Velazquez <juandvelaz@gmail.com>
2021-01-26 08:48:39 -0600

    Refactor: App.js is ready for presentation

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
-// COMPONENTS
-import AppScreen from './app/components/AppScreen';
-import AppTextInput from './app/components/AppTextInput';
-
 // SCREENS
 import AccountScreen from './app/screens/AccountScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
@@ -14,7 +10,12 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <MessagesScreen />;
+  return <WelcomeScreen />;
+  // return <ListingsScreen />;
+  // return <ListingDetailsScreen />;
+  // return <ViewImageScreen />;
+  // return <AccountScreen />;
+  // return <MessagesScreen />;
 }
 
 const styles = StyleSheet.create({});
