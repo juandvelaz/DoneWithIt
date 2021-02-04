d6afd0cfa9b8e2399dab6024d4a7ebd4e48a79a4
Juan Velazquez <juandvelaz@gmail.com>
2021-02-03 21:27:19 -0600

    Style: Updated AppTeextInput style to include Defaultstyles config

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import colors from '../config/colors';
 import defaultStyles from '../config/styles';
 
 function AppTextInput({ icon, ...otherProps }) {
@@ -12,7 +11,7 @@ function AppTextInput({ icon, ...otherProps }) {
         <MaterialCommunityIcons
           name={icon}
           size={20}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
           style={styles.icon}
         />
       )}
@@ -22,7 +21,7 @@ function AppTextInput({ icon, ...otherProps }) {
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: 'row',
     width: '100%',
